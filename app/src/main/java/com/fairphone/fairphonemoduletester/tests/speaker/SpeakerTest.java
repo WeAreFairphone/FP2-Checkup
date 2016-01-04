0869481f614376476136be0d631c3ad1d77857da
Maarten Derks <maarten@fairphone.com>
2016-01-04 10:58:17 +0100

    Fix volume

--- app/src/main/java/com/fairphone/fairphonemoduletester/tests/speaker/SpeakerTest.java
+++ app/src/main/java/com/fairphone/fairphonemoduletester/tests/speaker/SpeakerTest.java
@@ -1,6 +1,7 @@
 package com.fairphone.fairphonemoduletester.tests.speaker;
 
 import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,11 @@ import com.fairphone.fairphonemoduletester.tests.Test;
 public class SpeakerTest extends Test {
 
     View mTestView;
+
+    private AudioManager audioManager;
+    private int initMediaVolume;
+    private int maxMediaVolume;
+
     private MediaPlayer mediaPlayer;
 
     public SpeakerTest(Context context) {
@@ -23,6 +29,13 @@ public class SpeakerTest extends Test {
     @Override
     protected void runTest() {
         replaceView();
+
+        audioManager = (AudioManager)getContext().getSystemService(Context.AUDIO_SERVICE);
+        initMediaVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);                  // store current volume to restore later
+        maxMediaVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxMediaVolume / 2, 0);             // fix volume to 50%
+
         mediaPlayer = MediaPlayer.create(getContext(), R.raw.fiesta);
         mediaPlayer.setLooping(true);
         mediaPlayer.start();
@@ -30,6 +43,9 @@ public class SpeakerTest extends Test {
 
     @Override
     protected void onCleanUp() {
+        if (mediaPlayer != null) {
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, initMediaVolume, 0);            // restore original volume
+        }
         mediaPlayer.release();
         mediaPlayer = null;
         super.onCleanUp();
