29cd61a1f5fb63ec9ff673fa761ec1fad23e530d
Maarten Derks <maarten@fairphone.com>
2016-09-29 17:09:33 +0200

    Refactor magnetometer test

--- app/src/main/java/com/fairphone/diagnostics/tests/magnetometer/MagnetometerTest.java
+++ app/src/main/java/com/fairphone/diagnostics/tests/magnetometer/MagnetometerTest.java
@@ -12,24 +12,14 @@ import android.widget.TextView;
 import com.fairphone.diagnostics.R;
 import com.fairphone.diagnostics.tests.Test;
 
-/**
- * Created by maarten on 23/09/16.
- */
-
-public class MagnetometerTest extends Test implements SensorEventListener {
+public class MagnetometerTest extends Test {
 
     private static final String TAG = MagnetometerTest.class.getSimpleName();
 
+    SensorEventListener mSensorEventListener;
+    SensorManager mSensorManager;
+    Sensor mMagnetometer;
     View mTestView;
-    private TextView tvHeading;
-
-    private SensorManager mSensorManager;
-    private Sensor mMagnetometer;
-    private Sensor mAccelerometer;
-
-    private float[] mMagnetometerData;
-    private float[] mAccelerometerData;
-    private float[] mOrientation = new float[3];
 
     public MagnetometerTest(Context context) {
         super(context);
@@ -45,56 +35,55 @@ public class MagnetometerTest extends Test implements SensorEventListener {
         return R.string.magnetometer_test_description;
     }
 
-    protected void onPrepare() {
-        mSensorManager = ((SensorManager)getContext().getSystemService(Context.SENSOR_SERVICE));
-        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-    }
-
-    @Override
-    protected void runTest() {
-        replaceView();
-        tvHeading = (TextView)findViewById(R.id.tvHeading);
-
-        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_UI);
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
-
-        //askIfSuccess(getContext().getString(R.string.magnetometer_test_finish_question));
-    }
-
-    @Override
-    protected void onCleanUp() {
-        mSensorManager.unregisterListener(this);
-        super.onCleanUp();
-    }
-
     private void replaceView() {
         mTestView = LayoutInflater.from(getContext()).inflate(R.layout.view_magnetometer_test, null);
         setTestView(mTestView);
     }
 
-
     @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            mAccelerometerData = event.values;
-        }
-        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-            mMagnetometerData = event.values;
-        }
-        if ((mAccelerometerData != null) && (mMagnetometerData != null)) {
-            float R[] = new float[9];
-            float I[] = new float[9];
-            if (SensorManager.getRotationMatrix(R, I, mAccelerometerData, mMagnetometerData)) {
-                SensorManager.getOrientation(R, mOrientation);
-                float azimuth = mOrientation[0] * 57.29578F;
-                tvHeading.setText("Heading: " + azimuth);
-            }
-        }
+    protected void runTest() {
+        replaceView();
+        getMagnetometer();
+        setupSensorListener();
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    protected void onCleanUp() {
+        mSensorManager.unregisterListener(mSensorEventListener);
+        mSensorEventListener = null;
+        super.onCleanUp();
+    }
 
+    private void getMagnetometer() {
+        mSensorManager = (SensorManager)
+                getContext().getSystemService(Context.SENSOR_SERVICE);
+        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        if (mMagnetometer == null) {
+            onTestFailure();
+            return;
+        }
+    }
+
+    private void setupSensorListener() {
+        mSensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                    onSensorChange(event);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        mSensorManager.registerListener(mSensorEventListener, mMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private void onSensorChange(SensorEvent event) {
+        ((TextView)findViewById(R.id.xAxis)).setText("X-axis: " + event.values[0]);
+        ((TextView)findViewById(R.id.yAxis)).setText("Y-axis: " + event.values[1]);
+        ((TextView)findViewById(R.id.zAxis)).setText("Z-axis: " + event.values[2]);
     }
 }
