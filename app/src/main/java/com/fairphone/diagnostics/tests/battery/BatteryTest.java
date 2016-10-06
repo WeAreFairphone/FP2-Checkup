d3181d3f6ec34e6503d957a32791f7e81a3a66c3
Maarten Derks <maarten@fairphone.com>
2016-10-06 11:12:04 +0200

    Refactor battery test

--- app/src/main/java/com/fairphone/diagnostics/tests/battery/BatteryTest.java
+++ app/src/main/java/com/fairphone/diagnostics/tests/battery/BatteryTest.java
@@ -20,15 +20,8 @@ public class BatteryTest extends Test {
 
     private static final String TAG = BatteryTest.class.getSimpleName();
 
-    private TextView batteryHealth;
-    private TextView batteryLevel;
-    private TextView batteryStatus;
-    private TextView batteryTempature;
-    private TextView batteryVoltage;
-    //final Context context = this;
-    //private RelativeLayout layout;
-    //private Class tcNext = CameraKeyTest.class;
-    //private int testMode;
+    View mTestView;
+    BroadcastReceiver batteryStatusReceiver;
 
     public BatteryTest(Context context) {
         super(context);
@@ -45,79 +38,107 @@ public class BatteryTest extends Test {
     }
 
     protected void replaceView() {
-        View mTestView = LayoutInflater.from(getContext()).inflate(R.layout.view_battery_test, null);
+        mTestView = LayoutInflater.from(getContext()).inflate(R.layout.view_battery_test, null);
         setTestView(mTestView);
-    }
-
-
-    @Override
-    protected void onPrepare() {
-
     }
 
     @Override
     protected void runTest() {
         replaceView();
-
-        batteryLevel = (TextView)findViewById(R.id.tvBatteryLevel);
-        batteryStatus = ((TextView)findViewById(R.id.tvBatteryStatus));
-        batteryVoltage = ((TextView)findViewById(R.id.tvBatteryVolt));
-        batteryTempature = ((TextView)findViewById(R.id.tvBatteryTemperature));
-        batteryHealth = ((TextView)findViewById(R.id.tvBatteryHealth));
-
-        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        getContext().registerReceiver(batteryStatusReceiver, intentFilter);
-
-        //askIfSuccess(getResources().getString(R.string.battery_test_finish_question));
+        setupBroadcastReceiver();
     }
 
-    private BroadcastReceiver batteryStatusReceiver = new BroadcastReceiver() {
+    @Override
+    protected void onCleanUp() {
+        getContext().unregisterReceiver(batteryStatusReceiver);
+        batteryStatusReceiver = null;
+        super.onCleanUp();
+    }
+
+    private void setupBroadcastReceiver() {
+        IntentFilter ifilter = new IntentFilter();
+        ifilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        ifilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        ifilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        batteryStatusReceiver = new BatteryReceiver();
+        getContext().registerReceiver(batteryStatusReceiver, ifilter);
+    }
+
+    class BatteryReceiver extends BroadcastReceiver {
+        @Override
         public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)) {
-                batteryLevel.setText("Level: " + String.valueOf(intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)) + "%");
-                batteryVoltage.setText("Voltage: " + String.valueOf(intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0) / 1000.0F) + "V");
-                batteryTempature.setText("Temperature: " + String.valueOf(intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10.0F) + " °C");
 
-                int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, 1);
-                int batteryLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 1);
+            int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            switch(status) {
+                case BatteryManager.BATTERY_STATUS_UNKNOWN:
+                    ((TextView)findViewById(R.id.battery_status_value)).setText(getResources().getString(R.string.battery_status_unknown));
+                    break;
+                case BatteryManager.BATTERY_STATUS_CHARGING:
+                    ((TextView)findViewById(R.id.battery_status_value)).setText(getResources().getString(R.string.battery_status_charging));
+                    break;
+                case BatteryManager.BATTERY_STATUS_DISCHARGING:
+                    ((TextView)findViewById(R.id.battery_status_value)).setText(getResources().getString(R.string.battery_status_discharging));
+                    break;
+                case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
+                    ((TextView)findViewById(R.id.battery_status_value)).setText(getResources().getString(R.string.battery_status_not_charging));
+                    break;
+                case BatteryManager.BATTERY_STATUS_FULL:
+                    ((TextView)findViewById(R.id.battery_status_value)).setText(getResources().getString(R.string.battery_status_full));
+                    break;
+                default:
+                    ((TextView)findViewById(R.id.battery_status_value)).setText(getResources().getString(R.string.not_available));
+                    break;
+            }
 
-                String statusResult = "Unknown";
-                if(status == BatteryManager.BATTERY_STATUS_CHARGING) {
-                    statusResult = "Charging";
-                }
-                if (status == BatteryManager.BATTERY_STATUS_DISCHARGING || status == BatteryManager.BATTERY_STATUS_NOT_CHARGING) {
-                    statusResult = "Not Charging";
-                }
-                if ((status == BatteryManager.BATTERY_STATUS_FULL) || (batteryLevel == 100)) {
-                    statusResult = "Phone detected charger but unable to proceed with charging. Reason: Battery full charged";
-                }
-                batteryStatus.setText("Status: " + statusResult);
+            int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+            switch(plugged) {
+                case BatteryManager.BATTERY_PLUGGED_AC:
+                    ((TextView)findViewById(R.id.battery_plugged_value)).setText(getResources().getString(R.string.battery_plugged_ac));
+                    break;
+                case BatteryManager.BATTERY_PLUGGED_USB:
+                    ((TextView)findViewById(R.id.battery_plugged_value)).setText(getResources().getString(R.string.battery_plugged_usb));
+                    break;
+                case BatteryManager.BATTERY_PLUGGED_WIRELESS:
+                    ((TextView)findViewById(R.id.battery_plugged_value)).setText(getResources().getString(R.string.battery_plugged_wireless));
+                    break;
+                default:
+                    ((TextView)findViewById(R.id.battery_plugged_value)).setText("No");
+                    break;
+            }
 
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
 
-                int health = intent.getIntExtra(BatteryManager.EXTRA_HEALTH, 1);
+            ((TextView)findViewById(R.id.battery_level_value)).setText(level + getResources().getString(R.string.battery_level_unit));
 
-                String result;
-                switch (health) {
-                    case BatteryManager.BATTERY_HEALTH_GOOD:
-                        result = "Good";
-                        break;
-                    case BatteryManager.BATTERY_HEALTH_OVERHEAT:
-                        result = "Over Heat";
-                        break;
-                    case BatteryManager.BATTERY_HEALTH_DEAD:
-                        result = "Dead";
-                        break;
-                    case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
-                        result = "Over Voltage";
-                        break;
-                    case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
-                        result = "Unspecified Failure";
-                        break;
-                    default:
-                        result = "Unknown";
-                }
-                batteryHealth.setText("Health: " + result);
+            ((TextView)findViewById(R.id.battery_voltage_value)).setText(intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0) / 1000.0F + " " + getResources().getString(R.string.battery_voltage_unit));
+            ((TextView)findViewById(R.id.battery_temperature_value)).setText(intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10.0F + " " + getResources().getString(R.string.battery_temperature_unit));
+
+            int health = intent.getIntExtra(BatteryManager.EXTRA_HEALTH, -1);
+            switch (health) {
+                case BatteryManager.BATTERY_HEALTH_UNKNOWN:
+                    ((TextView)findViewById(R.id.battery_health_value)).setText(getResources().getString(R.string.battery_health_unkown));
+                    break;
+                case BatteryManager.BATTERY_HEALTH_GOOD:
+                    ((TextView)findViewById(R.id.battery_health_value)).setText(getResources().getString(R.string.battery_health_good));
+                    break;
+                case BatteryManager.BATTERY_HEALTH_OVERHEAT:
+                    ((TextView)findViewById(R.id.battery_health_value)).setText(getResources().getString(R.string.battery_health_overheat));
+                    break;
+                case BatteryManager.BATTERY_HEALTH_DEAD:
+                    ((TextView)findViewById(R.id.battery_health_value)).setText(getResources().getString(R.string.battery_health_dead));
+                    break;
+                case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
+                    ((TextView)findViewById(R.id.battery_health_value)).setText(getResources().getString(R.string.battery_health_over_voltage));
+                    break;
+                case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
+                    ((TextView)findViewById(R.id.battery_health_value)).setText(getResources().getString(R.string.battery_health_unspecified_failure));
+                    break;
+                case BatteryManager.BATTERY_HEALTH_COLD:
+                    ((TextView)findViewById(R.id.battery_health_value)).setText(getResources().getString(R.string.battery_health_cold));
+                    break;
+                default:
+                    ((TextView)findViewById(R.id.battery_health_value)).setText(getResources().getString(R.string.not_available));
             }
         }
-    };
+    }
 }
