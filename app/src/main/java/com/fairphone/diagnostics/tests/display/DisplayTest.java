f0faf15a50e1d34ec1f687bafe1d1cb16c3b530b
Maarten Derks <maarten@fairphone.com>
2016-10-20 14:42:59 +0200

    Add gradient pattern to LCD test.

--- app/src/main/java/com/fairphone/diagnostics/tests/display/DisplayTest.java
+++ app/src/main/java/com/fairphone/diagnostics/tests/display/DisplayTest.java
@@ -94,6 +94,7 @@ public class DisplayTest extends Test {
         mViewFlipper.addView(new FullColorTestView(getContext(), 0xff0000ff));
         mViewFlipper.addView(new FullColorTestView(getContext(), 0xffffffff));
         mViewFlipper.addView(new FullColorTestView(getContext(), 0xff000000));
+        mViewFlipper.addView(new GradientTestView(getContext()));
     }
 
     private void setTouchHandler() {
