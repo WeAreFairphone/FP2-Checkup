f47cbb863804d653bbd42cf43cc5ddce027f9a9a
Maarten Derks <maarten@fairphone.com>
2016-10-25 14:37:21 +0200

    Remove flicker from LCD test.

--- app/src/main/java/com/fairphone/checkup/tests/display/GradientTestView.java
+++ app/src/main/java/com/fairphone/checkup/tests/display/GradientTestView.java
@@ -2,6 +2,7 @@ package com.fairphone.checkup.tests.display;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 
 import com.fairphone.checkup.R;
@@ -11,18 +12,16 @@ import com.fairphone.checkup.R;
  */
 public class GradientTestView extends View {
 
-//    private GradientDrawable mDrawable;
+    private Drawable gradient;
 
     public GradientTestView(Context context) {
         super(context);
-//        mDrawable = new GradientDrawable(GradientDrawable.Orientation.BL_TR,
-//                new int[] {Color.RED, Color.MAGENTA, Color.BLUE, Color.CYAN, Color.GREEN, Color.YELLOW, Color.RED});
+        gradient = getResources().getDrawable(R.drawable.gradient);
+        setBackground(gradient);
     }
 
     protected void onDraw(Canvas canvas) {
         setWillNotDraw(false);
-//        setBackground(mDrawable);
-        setBackground(getResources().getDrawable(R.drawable.gradient));
     }
 }
 
