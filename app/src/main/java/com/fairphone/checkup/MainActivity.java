50c4c4e0e33235b87003cfb2c99908787c038a23
Borjan Tchakaloff <borjan@fairphone.com>
2016-11-13 23:54:02 +0100

    WIP: Generic speaker test to factor the ear and loud speakers
    The ear-speaker and the loud-speaker share most of the test.
    
    Change-Id: I9d6f641d964bf26a0237b55436719718c312f1a1

--- app/src/main/java/com/fairphone/checkup/MainActivity.java
+++ app/src/main/java/com/fairphone/checkup/MainActivity.java
@@ -13,6 +13,7 @@ import com.fairphone.checkup.tests.freedraw.FreeDrawTest;
 import com.fairphone.checkup.tests.lcd.LcdTest;
 import com.fairphone.checkup.tests.modem.ModemTest;
 import com.fairphone.checkup.tests.speaker.EarSpeakerTest;
+import com.fairphone.checkup.tests.speaker.LoudSpeakerTest;
 import com.fairphone.checkup.tests.vibrator.VibratorTest;
 
 import java.util.ArrayList;
@@ -37,6 +38,7 @@ public class MainActivity extends Activity implements TestListAdapter.OnClickLis
         displayTests.add(ModemTest.DETAILS);
         displayTests.add(VibratorTest.DETAILS);
         displayTests.add(EarSpeakerTest.DETAILS);
+        displayTests.add(LoudSpeakerTest.DETAILS);
 
         mListView = (ListView) findViewById(R.id.listView);
         mListView.setAdapter(new TestListAdapter(this, displayTests, this));
--- app/src/main/java/com/fairphone/checkup/tests/speaker/EarSpeakerTest.java
+++ app/src/main/java/com/fairphone/checkup/tests/speaker/EarSpeakerTest.java
@@ -11,9 +11,7 @@ import com.fairphone.checkup.R;
 import com.fairphone.checkup.tests.NewTest;
 import com.fairphone.checkup.tests.SimpleTest;
 
-public class EarSpeakerTest extends SimpleTest {
-
-    private static final String TAG = EarSpeakerTest.class.getSimpleName();
+public class EarSpeakerTest extends SpeakerTest {
 
     public static final Details DETAILS = new NewTest.Details(R.string.ear_speaker_test_title, R.string.ear_speaker_test_summary, R.string.ear_speaker_test_description, R.string.ear_speaker_test_instructions) {
         @Override
@@ -22,18 +20,8 @@ public class EarSpeakerTest extends SimpleTest {
         }
     };
 
-    /**
-     * The ratio to apply to the maximum stream volume.
-     */
-    private static final float MAX_STREAM_VOLUME_RATIO = 0.75f;
-
-    private AudioManager mAudioManager;
-    private MediaPlayer mMediaPlayer;
-
-    private int mOldStreamVolume;
-
     public EarSpeakerTest() {
-        super(true);
+        super(R.raw.fiesta, SPEAKER_EAR, 0.6f);
     }
 
     @Override
@@ -41,78 +29,4 @@ public class EarSpeakerTest extends SimpleTest {
         return DETAILS;
     }
 
-    @Override
-    protected void onCreateTest() {
-        super.onCreateTest();
-
-        mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
-
-        mMediaPlayer = MediaPlayer.create(
-                getActivity(),
-                R.raw.sunbeam,
-                new AudioAttributes.Builder()
-                        .setUsage(AudioAttributes.USAGE_VOICE_COMMUNICATION)
-                        .setContentType(AudioAttributes.CONTENT_TYPE_UNKNOWN)
-                        .build(),
-                0);
-
-        if (mMediaPlayer == null) {
-            // TODO cleanly fail
-            cancelTest();
-        } else {
-            mMediaPlayer.setLooping(true);
-        }
-    }
-
-    @Override
-    protected void onResumeTest(boolean firstResume) {
-        super.onResumeTest(firstResume);
-
-        if (mMediaPlayer == null) {
-            if (!isCancelled()) {
-                Log.e(TAG, "Missing MediaPlayer instance");
-            }
-            return;
-        }
-
-        final int maxStreamVolume = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL);
-        final int localStreamVolume = Math.min(Math.round(maxStreamVolume * MAX_STREAM_VOLUME_RATIO), maxStreamVolume);
-
-        mOldStreamVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_VOICE_CALL);
-
-        mAudioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
-        mAudioManager.setStreamVolume(AudioManager.STREAM_VOICE_CALL, localStreamVolume, 0);
-        mAudioManager.setStreamSolo(AudioManager.STREAM_VOICE_CALL, true);
-
-        mMediaPlayer.start();
-    }
-
-    @Override
-    protected void onPauseTest() {
-        super.onPauseTest();
-
-        if (mMediaPlayer == null) {
-            if (!isCancelled()) {
-                Log.e(TAG, "Missing MediaPlayer instance");
-            }
-            return;
-        }
-
-        mMediaPlayer.pause();
-
-        mAudioManager.setStreamSolo(AudioManager.STREAM_VOICE_CALL, false);
-        mAudioManager.setStreamVolume(AudioManager.STREAM_VOICE_CALL, mOldStreamVolume, 0);
-        mAudioManager.setMode(AudioManager.MODE_NORMAL);
-    }
-
-    @Override
-    protected void onFinishTest() {
-        super.onFinishTest();
-
-        if (mMediaPlayer != null) {
-            mMediaPlayer.reset();
-            mMediaPlayer.release();
-            mMediaPlayer = null;
-        }
-    }
 }
