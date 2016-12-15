d77302b86a2646f959d959a9348a91ef8b005de1
Borjan Tchakaloff <borjan@fairphone.com>
2016-12-15 19:39:41 +0100

    Extract the secret code to the resource file
    Change-Id: Ibab778cbbea090b05ea84c1e0c29b5baa76449bc

--- app/src/main/java/com/fairphone/checkup/SecretCodeReceiver.java
+++ app/src/main/java/com/fairphone/checkup/SecretCodeReceiver.java
@@ -5,12 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 
 public class SecretCodeReceiver extends BroadcastReceiver {
+    private static final String SECRET_ACTION = "android.provider.Telephony.SECRET_CODE";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        String secretAction = "android.provider.Telephony.SECRET_CODE";
         String action = intent.getAction();
         String host = intent.getData() != null ? intent.getData().getHost() : null;
-        if (secretAction.equals(action) && "3424".equals(host)) {
+
+        if (SECRET_ACTION.equals(action) && context.getString(R.string.secret_code_main_activity).equals(host)) {
             Intent activityIntent = new Intent(context, MainActivity.class);
             activityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
             context.startActivity(activityIntent);
