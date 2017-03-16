6cfbf2b04c0bac16e66fb7596441ffd4de44daeb
Borjan Tchakaloff <borjan@fairphone.com>
2017-03-16 18:55:47 +0100

    Fix the in-tree building
    The Support Library AppCompat was missing from the Android makefile.
    
    Change-Id: If0c4ae4e087dbb1373db63929f2bdf125b0ec8a8

--- Android.mk
+++ Android.mk
@@ -22,10 +22,14 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_JAVA_LIBRARIES := android-common
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 \
+                               android-support-v7-appcompat
 
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
 
-LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(RES_DIR))
+LOCAL_RESOURCE_DIR := frameworks/support/v7/appcompat/res \
+                      $(addprefix $(LOCAL_PATH)/, $(RES_DIR))
 
 LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main/java) $(call all-renderscript-files-under, app/src/main/java)
 LOCAL_SDK_VERSION := current
