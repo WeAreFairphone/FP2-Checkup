e10298c2027f5db2461df43c630beef6de67c37b
Borjan Tchakaloff <borjan@fairphone.com>
2017-04-24 16:05:02 +0200

    Simplify the WifiDetails class
    Rely on `WifiInfo` and `LinkProperties` class to hold the data.
    Update the way IP addresses are parsed (do not use a deprecated call).
    Remove the DHCP details.
    
    CHK-47
    
    Change-Id: Ic1fd4d499ba8e92cde568cc7c8177cb6f85b7d2e

--- app/src/main/java/com/fairphone/checkup/information/wifi/WifiDetails.java
+++ app/src/main/java/com/fairphone/checkup/information/wifi/WifiDetails.java
@@ -1,34 +1,41 @@
 package com.fairphone.checkup.information.wifi;
 
 import android.content.Context;
-import android.net.DhcpInfo;
+import android.net.LinkAddress;
+import android.net.LinkProperties;
 import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
 import android.os.Build;
-import android.text.format.Formatter;
 import android.util.Log;
 
 import com.fairphone.checkup.R;
 import com.fairphone.checkup.information.Information;
 
+import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
+/**
+ * A plain old Java object holding the Wi-FI network information.
+ * <p>At creation, the Wi-Fi network is not connected to a network ({@link #isConnected()} and the network information are not available ({@link R.string#not_available}).</p>
+ * <p>The public methods {@link #setDisabled()} and {@link #setEnabled(WifiInfo, LinkProperties)}} should be used to update an instance status.</p>
+ */
 public class WifiDetails extends Information.Details {
 
     private static final String TAG = WifiDetails.class.getSimpleName();
-
-    private static final String NO_BSSID = "00:00:00:00:00:00";
-    private static final String SIGNAL_STRENGTH_UNIT = "dBm";
 
     private final Context mContext;
     private final String mDataNotAvailableValue;
     private final String mWifiNetworkInterface;
 
     private WifiInfo mWifiInfo;
-    private DhcpInfo mDhcpInfo;
+    private LinkProperties mLinkProperties;
+
     private String mMacAddress;
+    private String mIpAddress;
+    private String mDnsServers;
 
     public WifiDetails(Context context) {
         mContext = context;
@@ -43,11 +50,12 @@ public class WifiDetails extends Information.Details {
      * <p>Use this method when the Wi-Fi is enabled.<br>
      * Some of the network-related fields are set (such as the MAC address) even when there is no active connection.</p>
      */
-    public void setEnabled(WifiInfo wifiInfo, DhcpInfo dhcpInfo) {
+    public void setEnabled(WifiInfo wifiInfo, LinkProperties linkProperties) {
         mWifiInfo = wifiInfo;
-        mDhcpInfo = dhcpInfo;
+        mLinkProperties = linkProperties;
 
         retrieveMacAddress();
+        retrieveIpAddresses();
     }
 
     /**
@@ -66,7 +74,7 @@ public class WifiDetails extends Information.Details {
     private void retrieveMacAddress() {
         if (mWifiInfo == null || Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             /*
-             * Either the interface is down or either we are running on Marshmallow and the mac
+             * Either the interface is down or either we are running on Marshmallow and the MAC
              * address is hidden.
              */
 
@@ -101,109 +109,70 @@ public class WifiDetails extends Information.Details {
         }
     }
 
+    private void retrieveIpAddresses() {
+        mIpAddress = mLinkProperties == null ? mDataNotAvailableValue : formatIpAddresses(mLinkProperties.getLinkAddresses());
+        mDnsServers = mLinkProperties == null ? mDataNotAvailableValue : formatIpAddresses(mLinkProperties.getDnsServers());
+    }
+
     /**
-     * Rely on the BSSID to determine if there is an active connection.
-     * <p>We do not want to rely on the SSID as it can be hidden or empty and the decodeing layer
-     * does not yield a consistent result.</p>
+     * @param addresses The list of objects ({@link InetAddress} or {@link LinkAddress}) to retrieve a human-readable form from.
+     * @return A human-readable string of IP addresses (comma-separated).
+     */
+    private String formatIpAddresses(List<?> addresses) {
+        if (addresses == null || addresses.size() == 0) {
+            return mDataNotAvailableValue;
+        }
+
+        final Iterator<?> iter = addresses.iterator();
+        final StringBuilder builder = new StringBuilder();
+        Object element;
+        while (iter.hasNext()) {
+            element = iter.next();
+
+            if (element instanceof InetAddress) {
+                builder.append(((InetAddress) element).getHostAddress());
+                if (iter.hasNext()) {
+                    builder.append(", ");
+                }
+            } else if (element instanceof LinkAddress) {
+                builder.append(element);
+                if (iter.hasNext()) {
+                    builder.append(", ");
+                }
+            }
+            // We do not handle other types
+        }
+
+        return builder.toString();
+    }
+
+    public WifiInfo getWifiInfo() {
+        return mWifiInfo;
+    }
+
+    public LinkProperties getLinkProperties() {
+        return mLinkProperties;
+    }
+
+    /**
+     * Rely on the link properties to determine if there is an active connection.
      *
      * @return Whether there is an active connection to a Wi-Fi network.
      */
     public boolean isConnected() {
-        return null != mWifiInfo && null != mWifiInfo.getBSSID() && !NO_BSSID.equals(mWifiInfo.getBSSID());
-    }
-
-    public String getSsid() {
-        return (null == mWifiInfo) ? mDataNotAvailableValue : mWifiInfo.getSSID();
-    }
-
-    /**
-     * @return Whether the connected SSID is hidden or <code>false</code> if not connected.
-     */
-    public boolean isSsidHidden() {
-        return null != mWifiInfo && mWifiInfo.getHiddenSSID();
-    }
-
-    public String getBssid() {
-        return (null == mWifiInfo) ? mDataNotAvailableValue : mWifiInfo.getBSSID();
+        return null != getLinkProperties();
     }
 
     public String getMacAddress() {
         return mMacAddress;
     }
 
-    /**
-     * @return The WiFi RSSI or <code>0</code> if not connected.
-     * @see #getSignalStrengthUnit()
-     */
-    public int getSignalStrength() {
-        return (null == mWifiInfo) ? 0 : mWifiInfo.getRssi();
-    }
-
-    public String getSignalStrengthUnit() {
-        return SIGNAL_STRENGTH_UNIT;
-    }
-
-    /**
-     * @return The signal level from <code>0</code> (bad connection or not connected) to <code>100</code>.
-     * @see WifiManager#calculateSignalLevel(int, int)
-     */
-    public int getSignalLevel() {
-        return (null == mWifiInfo) ? 0 : WifiManager.calculateSignalLevel(mWifiInfo.getRssi(), 100);
-    }
-
-    /**
-     * @return The link speed or <code>0</code> if not connected.
-     * @see #getLinkSpeedUnit()
-     */
-    public int getLinkSpeed() {
-        return (null == mWifiInfo) ? 0 : mWifiInfo.getLinkSpeed();
-    }
-
-    public String getLinkSpeedUnit() {
-        return WifiInfo.LINK_SPEED_UNITS;
-    }
-
-    /**
-     * @return The frequency or <code>0</code> if not connected.
-     * @see #getFrequencyUnit()
-     */
-    public int getFrequency() {
-        return (null == mWifiInfo) ? 0 : mWifiInfo.getFrequency();
-    }
-
-    public String getFrequencyUnit() {
-        return WifiInfo.FREQUENCY_UNITS;
-    }
-
     public String getIpAddress() {
-        return (null == mDhcpInfo) ? mDataNotAvailableValue : Formatter.formatIpAddress(mDhcpInfo.ipAddress);
+        return mIpAddress;
     }
 
-    public String getGatewayAddress() {
-        return (null == mDhcpInfo) ? mDataNotAvailableValue : Formatter.formatIpAddress(mDhcpInfo.gateway);
-    }
-
-    public String getNetmask() {
-        return (null == mDhcpInfo) ? mDataNotAvailableValue : Formatter.formatIpAddress(mDhcpInfo.netmask);
-    }
-
-    public String getDns1Address() {
-        return (null == mDhcpInfo) ? mDataNotAvailableValue : Formatter.formatIpAddress(mDhcpInfo.dns1);
-    }
-
-    public String getDns2Address() {
-        return (null == mDhcpInfo) ? mDataNotAvailableValue : Formatter.formatIpAddress(mDhcpInfo.dns2);
-    }
-
-    public String getServerAddress() {
-        return (null == mDhcpInfo) ? mDataNotAvailableValue : Formatter.formatIpAddress(mDhcpInfo.serverAddress);
-    }
-
-    /**
-     * @return The DHCP lease duration in seconds or <code>0</code> if not connected.
-     */
-    public int getLeaseDuration() {
-        return (null == mDhcpInfo) ? 0 : mDhcpInfo.leaseDuration;
+    public String getDnsServers() {
+        return mDnsServers;
     }
 
 }
